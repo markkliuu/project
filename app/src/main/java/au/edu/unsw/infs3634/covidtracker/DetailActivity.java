92e69bbd68742f84afa9631b65a16b7dfbb1e82d
markkliuu <markliuyixin@gmail.com>
2020-09-29 15:44:35 +1000

    1

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/DetailActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/DetailActivity.java
@@ -9,41 +9,76 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
 
+import java.util.ArrayList;
 
 
 public class DetailActivity extends AppCompatActivity {
     public static final String INTENT_MESSAGE = "au.edu.unsw.infs3634.covidtracker.intent_message";
+
+    private TextView mCountry;
+    private TextView mNewcases;
+    private TextView mNewDeath;
+    private TextView mTotalDeath;
+    private TextView mNewRecovered;
+    private TextView mTotalRecovered;
+    private TextView mSearch;
+    private TextView mTotalcases;
+
+
+    Intent intent = getIntent();
+    String countryCode = intent.getStringExtra(INTENT_MESSAGE);
+    private Object Intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+// initialise variables
+        mCountry = findViewById(R.id.tvCountry);
+        mNewcases = findViewById(R.id.tvNewCases);
+        mNewRecovered = findViewById(R.id.tvNewRecovered);
+        mNewDeath = findViewById(R.id.tvNewDeath);
+        mSearch = findViewById(R.id.tvSearch);
+        mTotalRecovered = findViewById(R.id.tvTotalRecovered);
+        mTotalDeath = findViewById(R.id.tvTotalDeath);
+        mTotalcases = findViewById(R.id.tvTotalCases);
 
     Intent intent = getIntent();
-    if(intent.hasExtra(INTENT_MESSAGE))
-
-    {
-        TextView detailMessage = findViewById(R.id.tvDetailMessage);
-        detailMessage.setText(intent.getStringExtra(INTENT_MESSAGE));
-    }
+    String countryCode = intent.getStringExtra(INTENT_MESSAGE);
 
 
     // detailMessage.setText(intent.getStringExtra(INTENT_MESSAGE));
 
 
-    Button button = findViewById(R.id.btShowVideo);
+    ArrayList<Country> countries = Country.getCountries();
+    for(final Country country : countries){
+        if (country.getCountryCode().equals(countryCode))
+        // update the textviews with the data of the country
+        setTitle(country.getCountryCode());
+        mCountry.setText(country.getCountry());
+        mNewcases.setText(String.valueOf(country.getNewConfirmed()));
+      //  mTotalCases
+        mNewDeath.setText(String.valueOf(country.getNewDeaths()));
+        mTotalDeath.setText(String.valueOf(country.getTotalDeaths()));
+        mTotalRecovered.setText(String.valueOf(country.getTotalRecovered()));
+        mNewRecovered.setText(String.valueOf(country.getNewRecovered()));
 
-    button.setOnClickListener(new View.OnClickListener()
-    {
-        @Override
-        public void onClick (View v){
-        DetailActivity.this.showVideo("https://www.youtube.com/watch?v=ieU8EGiTBa4");
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchCountry(country.getCountry());
+            }
+        });
+
     }
-    });
-}
-    private void showVideo(String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    }
+
+
+
+    private void searchCountry(String country) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com/search?q=covid " + country));
         startActivity(intent);
     }
 }
